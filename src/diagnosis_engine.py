19cc53f2742d7288b3175cb9fc52419e2c134bc7
Shalopaiepta <rkolesnikov2004@mail.ru>
2026-03-05 17:29:00 +0700

    Коммит №18: (РЕФАКТОРИНГ) Добавлен комментарий и todo к diagnose

--- src/diagnosis_engine.py
+++ src/diagnosis_engine.py
@@ -2,6 +2,15 @@ class DiagnosisEngine:
     def __init__(self):
         pass
     def diagnose(self, answers: list) -> str:
+        """
+        Ставит диагноз по списку ответов пациента
+
+        Args:
+            answers (list): список булевых ответов (True=да, False=нет)
+
+        Returns:
+            str: текст диагноза
+        """
         # todo: реализовать diagnose до конца
         if answers[0] == False and answers[1] == False:
             return "Здоров"
