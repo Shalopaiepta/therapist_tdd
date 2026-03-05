34c3c8fd075a1f931f8ef0f05df5e296f4e490db
Shalopaiepta <rkolesnikov2004@mail.ru>
2026-03-05 17:28:27 +0700

    Коммит №17: (ЗЕЛЁНЫЙ) diagnose — расширена заглушка для Здоров

--- src/diagnosis_engine.py
+++ src/diagnosis_engine.py
@@ -3,4 +3,6 @@ class DiagnosisEngine:
         pass
     def diagnose(self, answers: list) -> str:
         # todo: реализовать diagnose до конца
+        if answers[0] == False and answers[1] == False:
+            return "Здоров"
         return "Простуда"
