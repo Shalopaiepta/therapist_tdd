509855ba6f3139b08171b94c7b24e73e97b744cc
Shalopaiepta <rkolesnikov2004@mail.ru>
2026-03-05 16:35:40 +0700

    Коммит N4: (ЗЕЛЁНЫЙ) get_question(0)

--- src/symptom_database.py
+++ src/symptom_database.py
@@ -1,3 +1,6 @@
 class SymptomDatabase:
     def __init__(self):
         pass
+    def get_question(self, index:int)->str:
+        #todo: Надо сделать нормально get_question
+        return "У вас есть температура?"
