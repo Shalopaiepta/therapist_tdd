3e529a550a9826d5316050599aea6d8d98318687
Shalopaiepta <rkolesnikov2004@mail.ru>
2026-03-05 17:08:59 +0700

    Коммит №10: (ЗЕЛЁНЫЙ) get_size() — заглушка return 2

--- src/symptom_database.py
+++ src/symptom_database.py
@@ -8,4 +8,9 @@ class SymptomDatabase:
         Возвращает: str: текст вопроса
         """
         #todo: Надо сделать нормально get_question
+        if index==1:
+            return "У вас есть кашель?"
         return "У вас есть температура?"
+    def get_size(self) -> int:
+        # todo: реализовать get_size до конца
+        return 2
