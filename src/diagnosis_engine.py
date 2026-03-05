3e6899ca111aed1500e0ff384382fa54cd138b47
Shalopaiepta <rkolesnikov2004@mail.ru>
2026-03-05 17:26:08 +0700

    Коммит №15: (ЗЕЛЁНЫЙ) diagnose — заглушка return Простуда

--- src/diagnosis_engine.py
+++ src/diagnosis_engine.py
@@ -1,3 +1,6 @@
 class DiagnosisEngine:
     def __init__(self):
         pass
+    def diagnose(self, answers: list) -> str:
+        # todo: реализовать diagnose до конца
+        return "Простуда"
