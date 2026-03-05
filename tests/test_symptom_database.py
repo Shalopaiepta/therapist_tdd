fdb04e522fda7c442da9b22c90219ceef86700c7
Shalopaiepta <rkolesnikov2004@mail.ru>
2026-03-05 16:30:53 +0700

    Коммит N3: (КРАСНЫЙ) Добавлен тест get_question(0) возвращает строку

--- tests/test_symptom_database.py
+++ tests/test_symptom_database.py
@@ -12,6 +12,11 @@ class TestSymptomDatabase(unittest.TestCase):
     def test_symptom_database_creation(self):
         db = SymptomDatabase()
         self.assertIsNotNone(db)
+    def test_get_question_0_returns_string(self):
+        db = SymptomDatabase()
+        result = db.get_question(0)
+        self.assertIsInstance(result,str)
+        self.assertGreater(len(result),0)
 
 
 if __name__ == '__main__':
