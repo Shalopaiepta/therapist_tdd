bc5d145720a0b8dab7497e746e473ef1c8569fc7
Shalopaiepta <rkolesnikov2004@mail.ru>
2026-03-05 17:35:59 +0700

    Коммит №24: (КРАСНЫЙ) Добавлен тест ask_all() возвращает список длиной 2

--- tests/test_therapist.py
+++ tests/test_therapist.py
@@ -12,6 +12,11 @@ class TestTherapist(unittest.TestCase):
     def test_therapist_creation(self):
         therapist = Therapist()
         self.assertIsNotNone(therapist)
+    def test_ask_all_returns_list_of_correct_length(self):
+        therapist = Therapist()
+        result = therapist.ask_all([True, False])
+        self.assertIsInstance(result, list)
+        self.assertEqual(len(result), 2)
 
 
 if __name__ == '__main__':
