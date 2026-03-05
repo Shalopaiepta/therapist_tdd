3fadcbced86a57cb4547bc2d0077964ee0d26546
Shalopaiepta <rkolesnikov2004@mail.ru>
2026-03-05 17:36:40 +0700

    Коммит №26: (РЕФАКТОРИНГ) Добавлен комментарий к ask_all

--- src/therapist.py
+++ src/therapist.py
@@ -1,6 +1,16 @@
 class Therapist:
     def __init__(self):
         pass
+
     def ask_all(self, answers: list) -> list:
+        """
+        Принимает ответы пациента на все вопросы
+
+        Args:
+            answers (list): список булевых ответов (True=да, False=нет)
+
+        Returns:
+            list: список ответов
+        """
         # todo: реализовать ask_all до конца
         return [False, False]
