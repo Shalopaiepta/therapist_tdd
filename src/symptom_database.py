6a08b2b3f4c77dca0f119d32b003ba41d03597c2
Shalopaiepta <rkolesnikov2004@mail.ru>
2026-03-05 16:39:49 +0700

    Коммит N5: (РЕФАКТОРИНГ) Добавлен комментарий к get_question

--- src/symptom_database.py
+++ src/symptom_database.py
@@ -2,5 +2,10 @@ class SymptomDatabase:
     def __init__(self):
         pass
     def get_question(self, index:int)->str:
+        """
+        Возвращает вопрос по индексу из базы симптомов
+        Аргументы: index(int): индекс вопроса
+        Возвращает: str: текст вопроса
+        """
         #todo: Надо сделать нормально get_question
         return "У вас есть температура?"
